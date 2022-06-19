c60db64c8b7b3f0cac944fbfa4b89e2195b60aba
UADITYA8 <UADITYA8@hpADITYA>
2022-06-19 11:38:39 +0530

    first commit...

---
+++ FirstAppGit/src/FirstApp.java
@@ -0,0 +1,14 @@
+
+public class FirstApp {
+	public static void Main(String[] args)
+	{
+		System.out.println("first App");
+		int a=2;
+		int	b=3;
+		int c = a+b;
+		System.out.println(c);
+		
+		
+	}
+
+}
